5e6866934ef462adcd7f0b416e14eec90bf38fcb
m-vdb <mvergerdelbove@work4labs.com>
2016-03-10 21:47:53 -0800

    use dotenv in wsgi

--- buildservice/wsgi.py
+++ buildservice/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import dotenv
 
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "buildservice.settings")
 
 application = get_wsgi_application()
