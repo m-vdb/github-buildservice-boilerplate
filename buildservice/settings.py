864357f0575742330a42c75bf9918d4c90f335c9
m-vdb <mvergerdelbove@work4labs.com>
2016-03-11 05:52:03 -0800

    add comment on GITHUB_SCOPES

--- buildservice/settings.py
+++ buildservice/settings.py
@@ -143,6 +143,6 @@ STATIC_URL = '/static/'
 
 GITHUB_CLIENT_ID = os.environ.get('GITHUB_CLIENT_ID')
 GITHUB_CLIENT_SECRET = os.environ.get('GITHUB_CLIENT_SECRET')
-GITHUB_SCOPES = ["repo"]
+GITHUB_SCOPES = ["repo"]  # use "read:public_key" to checkout code if needed
 GITHUB_AUTHORIZATION_BASE_URL = "https://github.com/login/oauth/authorize"
 GITHUB_TOKEN_URL = "https://github.com/login/oauth/access_token"
