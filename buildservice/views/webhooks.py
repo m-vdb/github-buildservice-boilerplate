d10cea5f283fef3aba4c30e1fc2acd10512264ef
m-vdb <mvergerdelbove@work4labs.com>
2016-03-11 06:53:29 -0800

    firt draft for /webhook/create

--- buildservice/views/webhooks.py
+++ buildservice/views/webhooks.py
@@ -2,14 +2,30 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
+from buildservice.models import Webhook
+from buildservice.utils import github
+
 
 @login_required
 def create(request):
-    # TODO:
-    # create the webhooks + mark them as created in DB
-    # { "name: "web",
-    #   "config: {"url": "...", "content_type": "json", "secret": "...", "insecure_ssl": "0"},
-    #   "events: [pull_request"]}
+    repos = set()
+    for name, value in request.POST.iteritems():
+        if name.startswith('repo_') and value == 'on':
+            repos.add(name[5:])
+
+    repos_with_hook = request.user.webhook_set.filter(active=True).values_list('repository', flat=True)
+    new_repos = repos.difference(repos_with_hook)
+    if new_repos:
+        # TODO: catch that (decorator)
+        token = request.user.oauth_token
+        hooks = []
+        hook_url = request.build_absolute_uri(reverse('webhooks_pull_request'))
+        for repo in new_repos:
+            hook_id = github.create_webhook(token.value, repo, hook_url)
+            hooks.append(Webhook(repository=repo, github_id=hook_id))
+
+        request.user.webhook_set.add(*hooks)
+
     return redirect('home')
 
 
