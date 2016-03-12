cc3d1a53717153fee406e770305def8e1f444edf
m-vdb <mvergerdelbove@work4labs.com>
2016-03-12 10:40:23 -0800

    save the token in database

--- buildservice/views/oauth.py
+++ buildservice/views/oauth.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.shortcuts import redirect
 from requests_oauthlib import OAuth2Session
 
+from buildservice.models import OAuthToken
+
 
 def login(request):
     github = OAuth2Session(settings.GITHUB_CLIENT_ID, scope=settings.GITHUB_SCOPES)
@@ -19,4 +21,5 @@ def callback(request):
         authorization_response=request.build_absolute_uri()
     )
 
+    OAuthToken.objects.create(user=request.user, value=token)
     return redirect("interface_home")
