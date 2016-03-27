73ac9dc31c4c394a642c6b5209446e885d6f547c
m-vdb <mvergerdelbove@work4labs.com>
2016-03-27 14:38:00 -0700

    Repository.name is unique

--- buildservice/models/repository.py
+++ buildservice/models/repository.py
@@ -8,5 +8,5 @@ class Repository(models.Model):
     """
     A database record containing a repository.
     """
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
