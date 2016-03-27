93f398ca82120eae24a2dae89c7fd87051ce1bd3
m-vdb <mvergerdelbove@work4labs.com>
2016-03-27 14:35:02 -0700

    Repository model

---
+++ buildservice/models/repository.py
@@ -0,0 +1,12 @@
+"""The Repository model"""
+from django.db import models
+from django.conf import settings
+from django.core.urlresolvers import reverse
+
+
+class Repository(models.Model):
+    """
+    A database record containing a repository.
+    """
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
