e206ef4263fcdbadfb227d9d92f6e46a897cf27f
m-vdb <mvergerdelbove@work4labs.com>
2016-04-10 17:57:28 -0700

    fix context processor for anonymous users

--- buildservice/utils/context_processors.py
+++ buildservice/utils/context_processors.py
@@ -13,7 +13,9 @@ def base(request):  # pylint: disable=unused-argument
     A base context processor that return
     app-wide settings.
     """
-    hooks = request.user.webhook_set.filter(active=True)
+    user = request.user
+    # duck-typing
+    hooks = user.webhook_set.filter(active=True) if hasattr(user, 'webhook_set') else []
     return {
         "app_name": settings.BUILDSERVICE_APP_NAME,
         "user_repositories": sorted((hook.repository for hook in hooks), key=attrgetter('name'))
