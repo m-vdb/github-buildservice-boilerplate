27fc6ff6d2a7806ec5c7545d2c2eedc4e30fa81c
m-vdb <mvergerdelbove@work4labs.com>
2016-04-12 22:59:20 -0700

    Repository.get_token() method

--- buildservice/models/repository.py
+++ buildservice/models/repository.py
@@ -1,6 +1,9 @@
 """The Repository model"""
 from django.db import models
 
+from .oauth_token import OAuthToken
+from buildservice.errors import MissingToken
+
 
 class Repository(models.Model):
     """
@@ -10,3 +13,14 @@ class Repository(models.Model):
     default_branch = models.CharField(max_length=255, default='master')
     build_count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def get_token(self):
+        """
+        Get a token for the repository. If none
+        is found, a MissingToken exception is raised.
+        """
+        token = OAuthToken.objects.filter(user__webhook__repository=self).first()
+        if not token:
+            raise MissingToken('Cannot find token for repository %s' % self)
+
+        return token
