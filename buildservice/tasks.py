f646c7277191af21b68729bbb52e124b072a6383
m-vdb <mvergerdelbove@work4labs.com>
2016-04-12 23:03:51 -0700

    simplify the execute_build() task to use Build.update_status()

--- buildservice/tasks.py
+++ buildservice/tasks.py
@@ -1,9 +1,7 @@
 """Asynchronous tasks"""
 from django_rq import job
 
-from buildservice.errors import MissingToken
-from buildservice.models import Build, OAuthToken
-from buildservice.utils import github
+from buildservice.models import Build
 
 
 @job
@@ -12,16 +10,7 @@ def execute_build(build_id):
     Execute a given build.
     """
     build = Build.objects.get(pk=build_id)
-    token = OAuthToken.objects.filter(user__webhook__repository=build.repository).first()
-    if not token:
-        raise MissingToken('Cannot find token for build %s' % build_id)
 
-    # TODO: do stuff
-
+    # when build complete...
     status = 'success'
-    build.status = status
-    build.save()
-    github.create_status(
-        token.value, build.repository, build.sha,
-        state=status, target_url=build.url
-    )
+    build.update_status(status, token)
