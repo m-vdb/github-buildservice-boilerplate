5fcda57d8220ead24e5e47c5865112b2996a5c5a
m-vdb <mvergerdelbove@work4labs.com>
2016-04-15 07:08:36 -0700

    better error handling in the API

--- buildservice/views/api.py
+++ buildservice/views/api.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_POST
 
-from buildservice.errors import MissingToken
+from buildservice.errors import MissingToken, InvalidStatus
 from buildservice.models import Build
 from buildservice.utils.decorators import require_json
 
@@ -18,12 +18,14 @@ def update_build_status(request, repository_name, build_number):
     try:
         status = request.json['status']
     except (TypeError, KeyError):
-        return JsonResponse({'error': 'missing status field.'}, status=400)
+        return JsonResponse({'error': 'Missing status field.'}, status=400)
 
     build = get_object_or_404(Build, repository__name=repository_name, number=build_number)
     try:
         build.update_status(status)
+    except InvalidStatus as error:
+        return JsonResponse({'error': str(error)}, status=400)
     except MissingToken:
-        return JsonResponse({'error': 'no token'}, status=400)
+        return JsonResponse({'error': 'No token.'}, status=400)
 
     return JsonResponse({})
