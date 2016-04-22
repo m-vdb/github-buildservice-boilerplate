190bd8ea0330a078ea2d1c985ce3b08c1e77e655
m-vdb <mvergerdelbove@work4labs.com>
2016-04-22 07:45:51 -0700

    create_user_token() creates webhook optionally

--- buildservice/utils/testing.py
+++ buildservice/utils/testing.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from buildservice.models import OAuthToken, Webhook
 
 
-def create_user_token(repo, user=None):
+def create_user_token(repo=None, user=None):
     """
     Create a Django user and token. For that we have to
     mimic the registration of a webhook.
@@ -13,5 +13,6 @@ def create_user_token(repo, user=None):
         user_class = get_user_model()
         user = user_class.objects.create_user('uuu', password='ttt')
     token = OAuthToken.objects.create(user=user, value='the_token')
-    Webhook.objects.create(user=user, repository=repo)
+    if repo:
+        Webhook.objects.create(user=user, repository=repo)
     return token
