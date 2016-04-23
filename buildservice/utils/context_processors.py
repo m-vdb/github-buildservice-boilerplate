9e45051fbf62d3a86322c5daf0e12bc4965a10ba
m-vdb <mvergerdelbove@work4labs.com>
2016-04-23 11:26:05 -0700

    use get_user_active_repositories() in views

--- buildservice/utils/context_processors.py
+++ buildservice/utils/context_processors.py
@@ -7,16 +7,18 @@ from operator import attrgetter
 
 from django.conf import settings
 
+from .views import get_user_active_repositories
+
 
 def base(request):  # pylint: disable=unused-argument
     """
     A base context processor that return
     app-wide settings.
     """
-    user = request.user
-    # duck-typing
-    hooks = user.webhook_set.filter(active=True) if hasattr(user, 'webhook_set') else []
     return {
         "app_name": settings.BUILDSERVICE_APP_NAME,
-        "user_repositories": sorted((hook.repository for hook in hooks), key=attrgetter('name'))
+        "user_repositories": sorted(
+            get_user_active_repositories(request.user),
+            key=attrgetter('name')
+        )
     }
