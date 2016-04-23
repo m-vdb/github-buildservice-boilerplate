a273b3c944e02e096931917088d66c416c9dd687
m-vdb <mvergerdelbove@work4labs.com>
2016-04-23 10:58:28 -0700

    repository is linked to users now

--- buildservice/models/repository.py
+++ buildservice/models/repository.py
@@ -1,4 +1,5 @@
 """The Repository model"""
+from django.conf import settings
 from django.db import models
 
 from .oauth_token import OAuthToken
@@ -9,6 +10,11 @@ class Repository(models.Model):
     """
     A database record containing a repository.
     """
+    users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='repositories',
+        related_query_name='repository'
+    )
     name = models.CharField(max_length=255, unique=True)
     default_branch = models.CharField(max_length=255, default='master')
     build_count = models.IntegerField(default=0)
@@ -19,7 +25,7 @@ class Repository(models.Model):
         Get a token for the repository. If none
         is found, a MissingToken exception is raised.
         """
-        token = OAuthToken.objects.filter(user__webhook__repository=self).first()
+        token = OAuthToken.objects.filter(user__repositories=self).first()
         if not token:
             raise MissingToken('Cannot find token for repository %s' % self)
 
